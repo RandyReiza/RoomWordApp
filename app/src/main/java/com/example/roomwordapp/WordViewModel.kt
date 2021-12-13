bc8a6145d3ee88230022f05fa2e08229a483962d
Randy Reiza <randyreiza14@gmail.com>
2021-12-13 11:19:54 +0700

    add View Model Factory to passing constructor for ViewModel, because ViewModel class can't receive constructor (param) directly
    Note: Constructor (param) that usually sent is Repository

--- app/src/main/java/com/example/roomwordapp/WordViewModel.kt
+++ app/src/main/java/com/example/roomwordapp/WordViewModel.kt
@@ -20,3 +20,13 @@ class WordViewModel(private val repository: WordRepository) : ViewModel() {
         repository.insert(word)
     }
 }
+
+class WordViewModelFactory(private val repository: WordRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(WordViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return WordViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
