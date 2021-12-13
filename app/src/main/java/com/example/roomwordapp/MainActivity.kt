100bc4009dd6498cb763fe237719c8391250d4b0
Randy Reiza <randyreiza14@gmail.com>
2021-12-13 11:39:31 +0700

    create RecyclerView Adapter class to manage the data display, and use at MainActivity

--- app/src/main/java/com/example/roomwordapp/MainActivity.kt
+++ app/src/main/java/com/example/roomwordapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.roomwordapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomwordapp.adapter.WordListAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Implement the RecyclerView
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/com/example/roomwordapp/adapter/WordListAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.roomwordapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomwordapp.R
+import com.example.roomwordapp.database.Word
+
+class WordListAdapter : ListAdapter<Word, WordListAdapter.WordViewHolder>(WordsComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        return WordViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current.word)
+    }
+
+    // WordViewHolder allows us to bind a text to a TextView
+    class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val wordItemView: TextView = itemView.findViewById(R.id.textView)
+
+        fun bind(text: String?) {
+            wordItemView.text = text
+        }
+
+        companion object {
+            fun create(parent: ViewGroup): WordViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.recyclerview_item, parent, false)
+                return WordViewHolder(view)
+            }
+        }
+    }
+
+    // WordsComparator defines how to compute if two words are the same or if the contents are the same
+    class WordsComparator : DiffUtil.ItemCallback<Word>() {
+        override fun areItemsTheSame(oldItem: Word, newItem: Word): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Word, newItem: Word): Boolean {
+            return oldItem.word == newItem.word
+        }
+    }
+}
